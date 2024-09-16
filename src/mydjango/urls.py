79b3d83594d261c321a3104e788cc2133256c85e
Jose Roberto <joserobertomi@outlook.com>
2024-09-16 18:04:26 -0300

    criacao do api da API e helloworld de uma view

--- src/mydjango/urls.py
+++ src/mydjango/urls.py
@@ -13,7 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url 
+from django.urls import include, path
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
@@ -25,6 +26,7 @@ from myapp.views import ShowHelloWorld
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', ShowHelloWorld.as_view(), name='site'),
+    path('calendar_api/', include('calendar_api.urls')),
 ]
 # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
