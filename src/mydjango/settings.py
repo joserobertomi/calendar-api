6950f89c9276a26b49448edb96bc4485d5342a39
Jose Roberto <joserobertomi@outlook.com>
2024-09-17 00:29:10 -0300

    first migrate

--- src/mydjango/settings.py
+++ src/mydjango/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'calendar_api.apps.CalendarApiConfig',
     'myapp.apps.MyappConfig',
     'celery',
     'bootstrap5',
