e93bd0c19943c3bc267aceff95860266e9d88f1a
Jose Roberto <joserobertomi@outlook.com>
2024-09-17 00:39:47 -0300

    add: admin permision to models

--- src/calendar_api/admin.py
+++ src/calendar_api/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Endereco, Convenio, Paciente, SolicitacaoAgendamento, Profissional, ProfissionalProcedimento, Procedimento, HorariosAtendimento
+
+# Registre os modelos no Django Admin
+admin.site.register(Endereco)
+admin.site.register(Convenio)
+admin.site.register(Paciente)
+admin.site.register(SolicitacaoAgendamento)
+admin.site.register(Profissional)
+admin.site.register(ProfissionalProcedimento)
+admin.site.register(Procedimento)
+admin.site.register(HorariosAtendimento)
