442bcc76040ec6617879d2b63d93381aaa7b9012
Jose Roberto <joserobertomi@outlook.com>
2024-09-26 20:18:19 -0300

    add: views e urls finalizadas

--- src/calendar_api/urls.py
+++ src/calendar_api/urls.py
@@ -1,14 +1,27 @@
-from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import index, EnderecoViewSet  # Altere o caminho conforme necessário
+from .views import (
+    EnderecoViewSet, 
+    ConvenioViewSet, 
+    PacienteViewSet, 
+    SolicitacaoAgendamentoViewSet, 
+    ProfissionalViewSet, 
+    ProfissionalProcedimentoViewSet, 
+    ProcedimentoViewSet, 
+    HorariosAtendimentoViewSet
+)
 
-# Cria uma instância do DefaultRouter
+# Cria o roteador padrão
 router = DefaultRouter()
-# Registra o UserViewSet com o nome 'users'
-router.register(r'enderecos', EnderecoViewSet, basename='endereco')
 
+# Registra os ViewSets no roteador
+router.register(r'enderecos', EnderecoViewSet)
+router.register(r'convenios', ConvenioViewSet)
+router.register(r'pacientes', PacienteViewSet)
+router.register(r'solicitacoes-agendamento', SolicitacaoAgendamentoViewSet)
+router.register(r'profissionais', ProfissionalViewSet)
+router.register(r'profissionais-procedimentos', ProfissionalProcedimentoViewSet)
+router.register(r'procedimentos', ProcedimentoViewSet)
+router.register(r'horarios-atendimento', HorariosAtendimentoViewSet)
 
-urlpatterns = [
-    path('index/', index, name='index'),
-    path('', include(router.urls)), # Inclui as URLs do router
-]
+# Inclui as URLs geradas pelo roteador no padrão de rotas da aplicação
+urlpatterns = router.urls
--- src/calendar_api/views.py
+++ src/calendar_api/views.py
@@ -1,73 +1,45 @@
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
-from rest_framework import status
-from rest_framework.viewsets import ViewSet
-from rest_framework.response import Response
-from rest_framework.exceptions import NotFound
-from .serializers import EnderecoSerializer
-from .models import Endereco
+from rest_framework import viewsets
+from .serializers import EnderecoSerializer, ConvenioSerializer, PacienteSerializer, SolicitacaoAgendamentoSerializer, ProfissionalSerializer, ProfissionalProcedimentoSerializer, ProcedimentoSerializer, HorariosAtendimentoSerializer
+from .models import Endereco, Convenio, Paciente, SolicitacaoAgendamento, Profissional, ProfissionalProcedimento, Procedimento, HorariosAtendimento
 
 
 # Create your views here.
-# TODO VIEWSET --> Endereco, Paciente, Medico, HorarioAtendimento, Procedimento, Convenio, SolicitacaoAgendamento, ProfissionalProcedimento 
-# TODO --> Cadastro(CREATE=post), Listagem(LIST=get), Edicao(UPDATE=update), Vizualizacao(RETRIVE=get) 
 
-def index(request):
-    return HttpResponse("Hello, worldzin estoy a implementar a view")
+class EnderecoViewSet(viewsets.ModelViewSet):
+    queryset = Endereco.objects.all()
+    serializer_class = EnderecoSerializer
 
 
-class EnderecoViewSet(ViewSet):
+class ConvenioViewSet(viewsets.ModelViewSet):
+    queryset = Convenio.objects.all()
+    serializer_class = ConvenioSerializer
 
-    def create(self, request): 
-        # Deserializa os dados da requisição e verifica se são válidos
-        serializer = EnderecoSerializer(data=request.data)
-        
-        # Se os dados forem válidos, cria o objeto e retorna uma resposta de sucesso
-        if serializer.is_valid():
-            serializer.save()  # Salva o objeto no banco de dados
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
-        # Se os dados não forem válidos, retorna uma resposta de erro com os detalhes
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def update(self, request, pk=None):
-        # Tenta buscar o objeto que deve ser atualizado pelo `pk`
-        try:
-            endereco = Endereco.objects.get(pk=pk)
-        except Endereco.DoesNotExist:
-            raise NotFound(detail="Endereco nao encontrado")
 
-        # Deserializa e valida os dados recebidos
-        serializer = EnderecoSerializer(endereco, data=request.data)
-        
-        # Se os dados forem válidos, atualiza o objeto
-        if serializer.is_valid():
-            serializer.save()  # Atualiza o objeto no banco de dados
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        
-        # Se os dados não forem válidos, retorna uma resposta de erro
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def list(self, request):
-        queryset = Endereco.objects.all()
-        serializer = EnderecoSerializer(queryset, many=True)
-        return Response(serializer.data)
+class PacienteViewSet(viewsets.ModelViewSet):
+    queryset = Paciente.objects.all()
+    serializer_class = PacienteSerializer
 
-    def retrieve(self, request, pk=None):
-        queryset = Endereco.objects.all()
-        endereco = get_object_or_404(queryset, pk=pk)
-        serializer = EnderecoSerializer(endereco)
-        return Response(serializer.data)
-    
-    def destroy(self, request, pk=None):
-        # Tenta buscar o objeto que será deletado pelo `pk`
-        try:
-            user = Endereco.objects.get(pk=pk)
-        except Endereco.DoesNotExist:
-            raise NotFound(detail="User not found")
 
-        # Se o objeto for encontrado, deleta-o
-        user.delete()
-        
-        # Retorna uma resposta de sucesso
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class SolicitacaoAgendamentoViewSet(viewsets.ModelViewSet):
+    queryset = SolicitacaoAgendamento.objects.all()
+    serializer_class = SolicitacaoAgendamentoSerializer
+
+
+class ProfissionalViewSet(viewsets.ModelViewSet):
+    queryset = Profissional.objects.all()
+    serializer_class = ProfissionalSerializer
+
+
+class ProfissionalProcedimentoViewSet(viewsets.ModelViewSet):
+    queryset = ProfissionalProcedimento.objects.all()
+    serializer_class = ProfissionalProcedimentoSerializer
+
+
+class ProcedimentoViewSet(viewsets.ModelViewSet):
+    queryset = Procedimento.objects.all()
+    serializer_class = ProcedimentoSerializer
+
+
+class HorariosAtendimentoViewSet(viewsets.ModelViewSet):
+    queryset = HorariosAtendimento.objects.all()
+    serializer_class = HorariosAtendimentoSerializer
