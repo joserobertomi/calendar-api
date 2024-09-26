aa63672901df8e80e98e8e0378c47261cd1d1684
Jose Roberto <joserobertomi@outlook.com>
2024-09-26 19:42:32 -0300

    add: serilizers finalizados

--- src/calendar_api/serializers.py
+++ src/calendar_api/serializers.py
@@ -6,3 +6,45 @@ class EnderecoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Endereco
         fields = '__all__'
+
+
+class ConvenioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Convenio
+        fields = '__all__'
+
+
+class PacienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Paciente
+        fields = '__all__'
+
+
+class SolicitacaoAgendamentoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SolicitacaoAgendamento
+        fields = '__all__'
+
+
+class ProfissionalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profissional
+        fields = '__all__'
+
+
+class ProfissionalProcedimentoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProfissionalProcedimento
+        fields = '__all__'
+
+
+class ProcedimentoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Procedimento
+        fields = '__all__'
+
+
+class HorariosAtendimentoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HorariosAtendimento
+        fields = '__all__'
