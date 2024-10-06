50a1d17365f6b052f216d8a87d2dcb53bf3a8e64
Jose Roberto <joserobertomi@outlook.com>
2024-10-06 18:52:51 -0300

    add: (patch http method) update flow tests

--- src/calendar_api/tests.py
+++ src/calendar_api/tests.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.test import APITestCase
-from .models import Endereco, Convenio, Profissional, Procedimento, Paciente
+from .models import Endereco, Convenio, Paciente, SolicitacaoAgendamento, Profissional, ProfissionalProcedimento, Procedimento, HorariosAtendimento, Prontuario
 
 # Create your tests here.
 
@@ -50,11 +50,18 @@ class ApiTestCase(APITestCase):
             email = "adelaide.mundoquali@gmail.com",
             nascimento = "1986-06-25"
         )
-        self.profissional_id_fk = self.profissional.id
-        endereco_id_fk = self.endereco.id
-        convenio_id_fk = self.convenio.id
-        procedimento_id_fk = self.procedimento.id
-        paciente_id_fk = self.paciente.id
+        self.solicitacao_agendamento = SolicitacaoAgendamento.objects.create(
+            data_consulta = "2024-10-30",
+            hora_inicio_consulta = "16:00:00"
+        )
+        self.horarios_atendimento = HorariosAtendimento.objects.create(
+            dia_da_semana = '2a',
+            inicio = "08:00:00",
+            fim = "12:00:00"
+        )
+        self.profissional_procedimento = ProfissionalProcedimento.objects.create(
+            tempo_duracao = "00:30:00"
+        )
     
 
     # Funcoes de Listagem
@@ -79,7 +86,7 @@ class ApiTestCase(APITestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_list_procedimentos(self):
-        url = "/api/profissional/"
+        url = "/api/procedimento/"
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
@@ -349,6 +356,20 @@ class ApiTestCase(APITestCase):
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, 400)
 
+    def  test_erro_create_profissional_com_email_existente(self):
+        url = "/api/profissional/"
+        data = {
+            "nome": "Jose Roberto", 
+            "sobrenome": "Mendonça Inacio",
+            "cpf": "06733563101",
+            "uf_registro": "GO",
+            "n_registro": "A",
+            "tipo_registro": "CRM",
+            "email": "joilson@gmail.com"
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, 400)
+
     def test_erro_create_procedimento_sem_dado(self):
         url = "/api/procedimento/"
         data = {}
@@ -514,3 +535,91 @@ class ApiTestCase(APITestCase):
         }
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, 400)
+
+    def test_erro_create_paciente_data_nascimento_formato_incorreto(self):
+        url = "/api/paciente/"
+        data = {
+            "nome": "Ana Julia",
+            "sobrenome": "Mendoça Inacio",
+            "cpf": "06733563100",
+            "rg": "12997",
+            "orgao_expeditor": "SSP-GO",
+            "sexo": "F",
+            "celular": "62983107172",
+            "email": "ana.julias@hotmail.com",
+            "nascimento": "20-02-2006",
+            "endereco_fk": self.endereco.id,   
+            "convenio_fk": self.convenio.id
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, 400)
+
+    # Fluxo de updates 
+    def test_update_convenio(self):
+        url = f"/api/convenio/{self.convenio.id}/"
+        data = {
+            "nome": "Real Liberty SA",
+        }
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, 200)
+ 
+    def test_update_endereco(self):
+        url = f"/api/endereco/{self.endereco.id}/"
+        data = {
+            "numero": 90
+        }
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_profissional(self):
+        url = f"/api/profissional/{self.profissional.id}/"
+        data = {
+            "nome": "Jose Roberto",
+            "sobrenome": "Mendonça Inacio",
+        }
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_procedimento(self):
+        url = f"/api/procedimento/{self.procedimento.id}/"
+        data = {
+            "nome": "Cirurgia de Hemorroida com Enzimas Foliformes"
+        }
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_solicitacao_agendamento(self):
+        url = f"/api/solicitacoes-agendamento/{self.procedimento.id}/"
+        data = {
+            "data_consulta": "2024-11-23",
+            "hora_inicio_consulta": "15:30:00",
+        }
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_profissional_procedimento(self):
+        url = f"/api/profissional-procedimento/{self.profissional_procedimento.id}/"
+        data = {
+            "tempo_duracao": "00:45:00",
+        }
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_horario_atendimento(self):
+        url = f"/api/horarios-atendimento/{self.horarios_atendimento.id}/"
+        data = {
+            "dia_da_semana": "2a",
+            "inicio": "14:00:00",
+            "fim": "20:00:00",
+        }
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_paciente(self):
+        url = f"/api/paciente/{self.paciente.id}/"
+        data = {
+            "email": "jujubinha@gmail.com"
+        }
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, 200)
+    
